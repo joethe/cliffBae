c70aae427f1f4237ac7f14a62b0cb8e3d29e7b18
J-Ziegler <ziegl210@morris.umn.edu>
2016-04-09 12:05:16 -0500

    Refactor game.js

--- public/views/testgames/game.js
+++ public/views/testgames/game.js
@@ -4,49 +4,47 @@ canvas.width = screen.width - 20;
 canvas.height = 1000;
 var context = canvas.getContext("2d");
 
-var tile = new Image();
-tile.src = "assets/images/GGGGGG.png";
-
 var tiles = [];
 
 function makeBoard() {
     for (var i = 0; i < 100; i++) {
         tiles.push(new Image());
-        tiles[i].src = "assets/images/GGGGGG.png";
+        tiles[i].src = "assets/images/placeHolder.png";
     }
 }
 
-var speed = 5;
-
 var width = canvas.width;
 var height = canvas.height;
-
-function makeCharacter(character, color) {
-    "use strict";
-    //context.fillStyle = color;
-    //context.fillRect(character.x, character.y, character.width, character.height);
-
-}
 
 function clearCanvas() {
     "use strict";
     context.clearRect(0, 0, width, height);
 }
 
+function drawBoard() {
+    "use strict";
+    var tilesPerRow = 10;
+    var tileWidth = 120;
+    var tileHeight = 130;
+
+    var tileXMod = tileWidth * (29/30);
+    var tileYMod = .72 * tileHeight;
+
+    for (var i = 0; i < 100; i++) {
+        context.drawImage(
+            tiles[i],
+            (tileXMod * (i % tilesPerRow)) + ((tileXMod/2) * Math.floor((i/tilesPerRow)%2)),    // X position of the tile
+            (tileYMod * Math.floor(i/tilesPerRow)),                                             // Y position of the tile
+            tileWidth,                                                                          // Width scaling of the tile (pixels?)
+            tileHeight);                                                                        // Height scaling of the tile (pixels?)
+    }
+}
+
 function render() {
     "use strict";
     clearCanvas()
-    var tilesPerRow = 10;
-    var tileWidth = 120;
-    var tileHeight = 150;
+    drawBoard();
 
-    for (var i = 0; i < 100; i++) {
-        context.drawImage(tiles[i],
-                          (116 * (i % tilesPerRow)) + (58 * Math.floor((i/tilesPerRow)%2)),  // X position of the tile
-                          ((.72 * tileHeight) * Math.floor(i/tilesPerRow)),                    // Y position of the tile
-                          tileWidth,                  // Width scaling of the tile (pixels?)
-                          tileHeight);                 // Height scaling of the tile (pixels?)
-    }
 }
 
 function game() {
@@ -58,4 +56,4 @@ function game() {
 setInterval(function () {
     "use strict";
     game();
-}, 1000 / 60);
+}, 1000 / 200);
