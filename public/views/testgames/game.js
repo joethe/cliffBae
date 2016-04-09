1a01f6c20d67bba31e624c76667252df970c95b3
Henry Fellows <Henry.r.fellows@gmail.com>
2016-04-09 01:39:18 -0500

    Broke it. Take a look.

--- public/views/testgames/game.js
+++ public/views/testgames/game.js
@@ -7,9 +7,30 @@ window.onload = function() {
     Crafty.init(gameBoard.availWidth, gameBoard.availHeight, document.getElementById('game'));
     console.log("Game Width: " + width);
     console.log("Game Height: " + height);
-    Crafty.e('2D, DOM, Color, Fourway')
-        .attr({x: 0, y: 0, w: 100, h: 100})
-        .color('#696969')
-        .fourway(200);
-
+  //  Crafty.e('2D, DOM, Color, Fourway')
+  //      .attr({x: 0, y: 0, w: 100, h: 100})
+  //      .color('#696969')
+  //      .fourway(200);
+   Crafty.paths({ audio: "custom/audio/path/", images: "assets/images/" });
+   Crafty.load(assetsObj, // preload assets
+        function() { //when loaded
+              //  Crafty.scene("splash"); //go to splash scene
+              //  Crafty.audio.play("boop"); //Play the audio file
+              //  Crafty.e('2D, DOM, lazycat'); // create entity with sprite
+              Crafty.e('2D, tileP, Fourway')
+                  //.attr({x: 0, y: 0, w: 100, h: 100})
+                  //.color('#696969')
+                  .fourway(200);
+            },
+        function(e) { //progress
+              //Callback when an asset is loaded. Contains information about assets loaded
+            },
+        function(e) { //uh oh, error loading
+            }
+        );
 }
+
+
+var assetsObj = {
+    "images": {"tileP": ["tilePlaceholder.png"]}
+    };
