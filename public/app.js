7328d8fa8abb60d2f1c264ffeff7f56507b81a1a
Joseph <thele116@morris.umn.edu>
2016-04-08 21:50:34 -0500

    Set up initial webserver stuff. Lots of issues.

---
+++ public/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Example Module:
+// angular.module('nameOfModule', ['dependency1', 'dependency2', 'dependency3']);
+// Dependencies will be automatically loaded for the modules that list them as requirements.
+
+angular.module('stdControllers', []);
+angular.module("mainApp", ['stdControllers']);
+
+
---
+++ public/views/main/main.controller.js
@@ -0,0 +1,8 @@
+//==================== MAIN CONTROLLER ==================================
+    angular.module('mainApp').controller('mainCtrl', function(){
+        var mainControl = this;
+        console.log("Main controller loaded!");
+
+        mainControl.textField = "";
+
+    });
