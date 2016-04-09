1eef5fd5266d1c5750c35f607e2e7af1bf092cb4
J-Ziegler <ziegl210@morris.umn.edu>
2016-04-09 01:22:22 -0500

    Minor game.js improvements

--- public/views/testgames/game.js
+++ public/views/testgames/game.js
@@ -1,12 +1,15 @@
 window.onload = function() {
 
     var gameBoard = document.getElementById("game");
+    var height = gameBoard.availHeight;
+    var width = gameBoard.availWidth;
+
     Crafty.init(gameBoard.availWidth, gameBoard.availHeight, document.getElementById('game'));
-    console.log("Screen Width: " + screen.availWidth);
-    console.log("Screen Height: " + screen.availHeight);
+    console.log("Game Width: " + width);
+    console.log("Game Height: " + height);
     Crafty.e('2D, DOM, Color, Fourway')
         .attr({x: 0, y: 0, w: 100, h: 100})
         .color('#696969')
         .fourway(200);
-        
+
 }
