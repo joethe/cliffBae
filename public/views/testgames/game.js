a1e6a138b06a0d004f1416bf81169d6a67d90d83
J-Ziegler <ziegl210@morris.umn.edu>
2016-04-09 11:46:23 -0500

    Create game board

--- public/views/testgames/game.js
+++ public/views/testgames/game.js
@@ -1,42 +1,31 @@
 /*jslint browser:true */
 var canvas = document.getElementById("game");
 canvas.width = screen.width - 20;
-canvas.height = screen.height - 250;
+canvas.height = 1000;
 var context = canvas.getContext("2d");
 
 var tile = new Image();
-tile.src = "assets/images/tilePlaceholder.png"
+tile.src = "assets/images/GGGGGG.png";
 
-var keys = [];
+var tiles = [];
+
+function makeBoard() {
+    for (var i = 0; i < 100; i++) {
+        tiles.push(new Image());
+        tiles[i].src = "assets/images/GGGGGG.png";
+    }
+}
+
+var speed = 5;
 
 var width = canvas.width;
 var height = canvas.height;
-
-var characterHeight = 100;
-var characterWidth = 100;
-
-var player = {
-    x: 40,
-    y: 40,
-    width: characterWidth,
-    height: characterHeight
-};
-
-window.addEventListener("keydown", function (e) {
-    "use strict";
-    keys[e.keyCode] = true;
-}, false);
-
-window.addEventListener("keyup", function (e) {
-    "use strict";
-    delete keys[e.keyCode];
-}, false);
 
 function makeCharacter(character, color) {
     "use strict";
     //context.fillStyle = color;
     //context.fillRect(character.x, character.y, character.width, character.height);
-    context.drawImage(tile, character.x, character.y, 100, 100)
+
 }
 
 function clearCanvas() {
@@ -44,54 +33,25 @@ function clearCanvas() {
     context.clearRect(0, 0, width, height);
 }
 
-function bounds(character, distance) {
-    "use strict";
-    if (character.x <= distance) {
-        character.x = distance;
-    }
-    if (character.y <= distance) {
-        character.y = distance;
-    }
-    if (character.x >= (width - character.width - distance)) {
-        character.x = width - character.width - distance;
-    }
-    if (character.y >= (height - character.height - distance)) {
-        character.y = height - character.height - distance;
-    }
-}
-
-function keyMovement() {
-    "use strict";
-    if (keys[38]) {
-        player.y -= speed;
-    }
-    if (keys[40]) {
-        player.y += speed;
-    }
-    if (keys[37]) {
-        player.x -= speed;
-    }
-    if (keys[39]) {
-        player.x += speed;
-    }
-}
-
 function render() {
     "use strict";
-    clearCanvas();
-    makeCharacter(player, "rgb(21, 206, 246)");
-}
+    clearCanvas()
+    var tilesPerRow = 10;
+    var tileWidth = 120;
+    var tileHeight = 150;
 
-
-function update() {
-    "use strict";
-    keyMovement();
-    bounds(player, 0);
+    for (var i = 0; i < 100; i++) {
+        context.drawImage(tiles[i],
+                          (116 * (i % tilesPerRow)) + (58 * Math.floor((i/tilesPerRow)%2)),  // X position of the tile
+                          ((.72 * tileHeight) * Math.floor(i/tilesPerRow)),                    // Y position of the tile
+                          tileWidth,                  // Width scaling of the tile (pixels?)
+                          tileHeight);                 // Height scaling of the tile (pixels?)
+    }
 }
 
 function game() {
     "use strict";
-    update();
+    makeBoard();
     render();
 }
 
